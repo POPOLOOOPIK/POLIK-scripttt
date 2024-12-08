106903a82154489d0093fcf62141c51bab0ee157
POPOLOOOPIK <elokaka228228@gmail.com>
2024-12-08 17:38:04 +0700

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detroi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Delete the cube
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
