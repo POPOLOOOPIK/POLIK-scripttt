66f3b92a9c04872197f1cf7ad973f17bef4a3451
POPOLOOOPIK <elokaka228228@gmail.com>
2024-12-08 17:18:10 +0700

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //output Hello, World! into console.
+        print("Hello, Word!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
